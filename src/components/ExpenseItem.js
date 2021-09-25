69fc3c4b95a2c1ad2c3d81dcff1290258428418c
DannyPadron09 <danny.padron09@gmail.com>
2021-09-25 12:44:29 -0400

    added in const to make expense info dynamic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+    const ExpenseDate = new Date(2021, 8, 25);
+    const ExpenseTitle = 'Car Insurance';
+    const ExpenseAmount = 300;
+
     return (
         <div className = "expense-item">
-            <div>Date</div>
+            <div>{ExpenseDate.toISOString()}</div>
             <div className = "expense-item__description">
-                <h2>Expense Description</h2>
-                <div className = "expense-item__price">Amount</div>
+                <h2>{ExpenseTitle}</h2>
+                <div className = "expense-item__price">{ExpenseAmount}</div>
             </div>
         </div>
     );
