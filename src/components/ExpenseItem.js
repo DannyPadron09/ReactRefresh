7d7b47a2805617e25dd51660b91cd207593c7f48
DannyPadron09 <danny.padron09@gmail.com>
2021-09-25 12:33:04 -0400

    placeholders created

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,11 @@
 function ExpenseItem() {
     return (
         <div>
-            <h2>Expense Item!</h2>
+            <div>Date</div>
+            <div>
+                <h2>Title</h2>
+                <div>Amount</div>
+            </div>
         </div>
     );
 }
