95e721170e6f1d4ee3804a7d8694ad1ce98a5f71
DannyPadron09 <danny.padron09@gmail.com>
2021-10-11 21:44:46 -0400

    moved jsx to new component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,11 +5,7 @@ function ExpenseItem(props) {
     
     return (
         <div className = "expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+            
             <div className = "expense-item__description">
                 <h2>{props.title}</h2>
                 <div className = "expense-item__price">{props.amount}</div>
