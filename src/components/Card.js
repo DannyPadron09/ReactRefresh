4de77fb407f1bbf790426a046363f5d146937383
DannyPadron09 <danny.padron09@gmail.com>
2021-10-19 20:08:47 -0400

    adding props to new component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,11 @@
 import './Card.css';
 
-function Card() {
+function Card(props) {
     
     return(
-        <div className = "card"></div>
+        <div className = "card">{props.children}</div>
     );
 }
+
 
 export default Card;
