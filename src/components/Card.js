078719e538e2a09def943cd79d210956efc7df49
DannyPadron09 <danny.padron09@gmail.com>
2021-10-23 10:56:38 -0400

    created const for new card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,10 @@ import './Card.css';
 
 function Card(props) {
     
+    const classes = 'card' + props.className;
+    
     return(
-        <div className = "card">{props.children}</div>
+        <div className = {classes}>{props.children}</div>
     );
 }
 
